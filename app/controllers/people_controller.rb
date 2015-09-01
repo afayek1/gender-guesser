87598a4258130cb4f0af0e22513668886d7ff2c5
afayek1 <afayek88@gmail.com>
2015-09-01 15:28:47 -0700

    add ajax call

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -29,6 +29,7 @@ class PeopleController < ApplicationController
     respond_to do |format|
       if @person.save
         format.html { redirect_to @person, notice: 'Person was successfully created.' }
+        format.js {}
         format.json { render :show, status: :created, location: @person }
       else
         format.html { render :new }
